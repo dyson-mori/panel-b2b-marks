919b1d09134e93576af6457f0fc12c13f6b992d0
dyson-mori <supp.programming@gmail.com>
2024-11-10 23:26:03 -0300

    images

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,37 @@
+import { ReactComponent as Tag } from './tag.svg';
+import { ReactComponent as Link } from './link.svg';
+import { ReactComponent as TextingLeft } from './texting-left.svg';
+import { ReactComponent as Coin } from './coin.svg';
+import { ReactComponent as Buildings } from './buildings.svg';
+import { ReactComponent as Box } from './box.svg';
+import { ReactComponent as Brush } from './brush.svg';
+import { ReactComponent as Image } from './image.svg';
+import { ReactComponent as Add } from './add.svg';
+import { ReactComponent as BarCode } from './barcode.svg';
+import { ReactComponent as Lock } from './lock.svg';
+import { ReactComponent as PersonalCard } from './personal-card.svg';
+import { ReactComponent as MoneyReceive } from './money-receive.svg';
+import { ReactComponent as Graph } from './graph.svg';
+import { ReactComponent as Logout } from './logout.svg';
+import { ReactComponent as AddBox } from './box-add.svg';
+import { ReactComponent as People } from './people.svg';
+
+export {
+  Tag,
+  Link,
+  Coin,
+  TextingLeft,
+  Buildings,
+  Box,
+  Brush,
+  Image,
+  Add,
+  BarCode,
+  Lock,
+  PersonalCard,
+  MoneyReceive,
+  Graph,
+  Logout,
+  AddBox,
+  People
+}
