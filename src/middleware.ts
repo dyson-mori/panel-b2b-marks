209f3c82dab6c8ad8c036da31c103f5eeddb41e8
dyson-mori <supp.programming@gmail.com>
2024-11-10 23:13:53 -0300

    auth

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const getAllCookies = await cookies();
+  const currentUser = getAllCookies.get('current-user')?.value;
+
+  if (currentUser && request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/pages/*', request.url));
+  };
+
+  if (!currentUser && request.nextUrl.pathname.startsWith('/pages')) {
+    return NextResponse.redirect(new URL('/', request.url));
+  };
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
