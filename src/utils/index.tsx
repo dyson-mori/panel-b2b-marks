808de805ea48257e8e4743e4e010e1f9d34d0847
dyson-mori <supp.programming@gmail.com>
2024-11-10 23:25:25 -0300

    upload to cloudinary

---
+++ src/utils/index.tsx
@@ -0,0 +1,22 @@
+import { cloudinary } from "src/services";
+
+export const uploadToCloudinary = async (fileUri: string, fileName: string) =>
+  cloudinary.uploader
+    .upload(
+      fileUri, {
+        invalidate: true,
+        resource_type: "auto",
+        filename_override: fileName,
+        folder: `marks-joias`, // any sub-folder name in your cloud
+        // folder: `community/upload-test`, // any sub-folder name in your cloud
+        use_filename: true,
+      }
+    )
+      .then(
+        data => ({
+          secure_url: data.secure_url,
+          width: data.width,
+          height: data.height,
+          public_id: data.public_id
+        })
+      )
